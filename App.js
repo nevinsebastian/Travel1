c1cad2d6c2105289457d9047e4fad47a18545f6f
nevin <nevin@pop-os.localdomain>
2023-09-17 02:05:25 +0530

    backend

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+
 import React, { useState } from "react";
 import {
   StyleSheet,
@@ -6,7 +7,8 @@ import {
   Dimensions,
   TextInput,
   Pressable,
-  KeyboardAvoidingView
+  KeyboardAvoidingView,
+  Platform,
 } from "react-native";
 import styles from "./styles";
 import Svg, { Image, Ellipse, ClipPath } from "react-native-svg";
@@ -15,11 +17,14 @@ import Animated, {
   useAnimatedStyle,
   interpolate,
   withTiming,
-  withDelay,
   runOnJS,
+  withSpring,
+  withDelay,
   withSequence,
-  withSpring
 } from "react-native-reanimated";
+
+// Import Axios for making HTTP requests
+import axios from 'axios';
 
 export default function App() {
   const { height, width } = Dimensions.get("window");
@@ -34,9 +39,7 @@ export default function App() {
       [-height / 2, 0]
     );
     return {
-      transform: [
-        { translateY: withTiming(interpolation, { duration: 1000 }) },
-      ],
+      transform: [{ translateY: withTiming(interpolation, { duration: 1000 }) }],
     };
   });
 
@@ -44,9 +47,7 @@ export default function App() {
     const interpolation = interpolate(imagePosition.value, [0, 1], [250, 0]);
     return {
       opacity: withTiming(imagePosition.value, { duration: 500 }),
-      transform: [
-        { translateY: withTiming(interpolation, { duration: 1000 }) },
-      ],
+      transform: [{ translateY: withTiming(interpolation, { duration: 1000 }) }],
     };
   });
 
@@ -54,11 +55,11 @@ export default function App() {
     const interpolation = interpolate(imagePosition.value, [0, 1], [180, 360]);
     return {
       opacity: withTiming(imagePosition.value === 1 ? 0 : 1, { duration: 800 }),
-      transform: [
-        { rotate: withTiming(interpolation + "deg", { duration: 1000 }) },
-      ],
+      transform: [{ rotate: withTiming(interpolation + "deg", { duration: 1000 }) }],
     };
   });
+
+  
 
   const formAnimatedStyle = useAnimatedStyle(() => {
     return {
@@ -71,9 +72,42 @@ export default function App() {
 
   const formButtonAnimatedStyle = useAnimatedStyle(() => {
     return {
-      transform: [{scale: formButtonScale.value}]
+      transform: [{ scale: formButtonScale.value }],
+    };
+  });
+
+  const registerUser = async () => {
+    // Gather user input from text inputs
+    const email = "nevin@example.com"; // Replace with the actual user input
+    const password = "keepmecloss"; // Replace with the actual user input
+    const confirm_password = "keepmecloss"; // Replace with the actual user input
+  
+    // Create the request body
+    const requestBody = {
+      email,
+      password,
+      confirm_password,
+    };
+  
+    try {
+      // Make a POST request to the backend API
+      const response = await axios.post("http://127.0.0.1:8000/users/", requestBody, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+  
+      // Log the response
+      console.log("Response:", response);
+  
+      // Handle the response (e.g., show a success message to the user)
+      console.log("Registration successful", response.data);
+    } catch (error) {
+      // Handle any errors (e.g., display an error message)
+      console.error("Registration failed", error);
     }
-  })
+  };
+  
 
   const loginHandler = () => {
     imagePosition.value = 0;
@@ -91,32 +125,34 @@ export default function App() {
 
   return (
     <KeyboardAvoidingView
-    style={{ flex: 1 }}
-    behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-    enabled
-  >
-    <Animated.View style={styles.container}>
-      <Animated.View style={[StyleSheet.absoluteFill, imageAnimatedStyle]}>
-        <Svg height={height + 100} width={width}>
-          <ClipPath id="clipPathId">
-            <Ellipse cx={width / 2} rx={height} ry={height + 100} />
-          </ClipPath>
-          <Image
-            href={require("./assets/ideogram.jpeg")}
-            width={width + 100}
-            height={height + 100}
-            preserveAspectRatio="xMidYMid slice"
-            clipPath="url(#clipPathId)"
-          />
-        </Svg>
-        <Animated.View
-          style={[styles.closeButtonContainer, closeButtonContainerStyle]}
-        >
-          <Text onPress={() => (imagePosition.value = 1)}>X</Text>
+      style={{ flex: 1 }}
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      enabled
+    >
+      <Animated.View style={styles.container}>
+        <Animated.View style={[StyleSheet.absoluteFill, imageAnimatedStyle]}>
+          <Svg height={height + 100} width={width}>
+            <ClipPath id="clipPathId">
+              <Ellipse cx={width / 2} rx={height} ry={height + 100} />
+            </ClipPath>
+            <Image
+              href={require("./assets/ideogram.jpeg")}
+              width={width + 100}
+              height={height + 100}
+              preserveAspectRatio="xMidYMid slice"
+              clipPath="url(#clipPathId)"
+            />
+          </Svg>
+          <Animated.View
+            style={[styles.closeButtonContainer, closeButtonContainerStyle]}
+          >
+            <Text onPress={() => (imagePosition.value = 1)}>X</Text>
+          </Animated.View>
         </Animated.View>
-      </Animated.View>
-      <View style={styles.bottomContainer}>
-        <Animated.View style={buttonsAnimatedStyle}>
+        <View style={styles.bottomContainer}>
+          <Animated.View style={buttonsAnimatedStyle}>
+
+
           <Pressable style={styles.button} onPress={loginHandler}>
             <Text style={styles.buttonText}>LOG IN</Text>
           </Pressable>
